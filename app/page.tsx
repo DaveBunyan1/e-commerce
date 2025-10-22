07116687ae7664bb9e01457b63d9bc1c0fd507df
Dave Bunyan <davebunyan1@gmail.com>
2025-10-22 13:43:09 -0400

    Created initial home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
-    </div>
+    <>
+      <nav className="flex items-center justify-between bg-white dark:bg-neutral-900 p-4 md:px-16 lg:px-32">
+        <div>Logo</div>
+        <ul className="flex gap-x-4">
+          <li>Home</li>
+          <li>Shop</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+        <ul>
+          <li>Account</li>
+        </ul>
+      </nav>
+      <main className="bg-white dark:bg-black w-full h-full flex flex-col gap-y-32 items-center p-4">
+        <section>Banner</section>
+        <section>Featured Products</section>
+        <section>Popular Products</section>
+        <section>Call To Action</section>
+        <footer>Footer</footer>
+      </main>
+    </>
   );
 }
